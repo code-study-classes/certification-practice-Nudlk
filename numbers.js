3c52a7e845f040c71e253afeb666f550c4f5c8b0
Nudlok <dad.gubin@yandex.ru>
2024-02-10 15:22:49 +0300

    upload Numbers.js

--- numbers.js
+++ numbers.js
@@ -1,1 +1,15 @@
+// Задание 1
+function calculateDistance(x1, x2) {
+  return Math.abs(x2 - x1);
+}
 
+console.log(calculateDistance(3, 7));
+export { calculateDistance };
+// Задание 2
+// Задание 3
+function calculateKilobytes(fileSizeInBytes) {
+  return Math.abs(fileSizeInBytes / 1024);
+}
+
+console.log(5000);
+export { calculateKilobytes };

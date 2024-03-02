67877ba8d1092297ae2543dcb4775e085cef8d8c
Nudlk <159611741+Nudlk@users.noreply.github.com>
2024-03-02 15:30:14 +0300

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -12,7 +12,7 @@ function calculateSegmentProduct(A, B, C) {
 console.log(calculateSegmentProduct(0, 5, 2));
 // Задание 3
 function calculateKilobytes(fileSizeInBytes) {
-return Math.abs(fileSizeInBytes / 1024);
+return Math.floor(fileSizeInBytes / 1024);
 }
 
 console.log(calculateKilobytes(5000));
@@ -24,17 +24,17 @@ function calculateSegments(lengthA, lengthB) {
 console.log(calculateSegments(10, 3))
 // Задание 5
 function calculateDigitSum(twoDigitNumber) {
-  first_num = Math.floor(twoDigitNumber % 10)
-  second_num = Math.floor(twoDigitNumber / 10)
+  let first_num = Math.floor(twoDigitNumber % 10)
+  let second_num = Math.floor(twoDigitNumber / 10)
   return Math.abs(first_num + second_num);
 }
 
 console.log(calculateDigitSum(47));
 // Задание 6
 function swapHundredsAndTens(treeDigitNumber) {
-  num_A = Math.floor(treeDigitNumber / 100)
-  num_B = Math.floor((treeDigitNumber / 10) % 10)
-  num_C = Math.floor(treeDigitNumber % 10)
+  let num_A = Math.floor(treeDigitNumber / 100)
+  let num_B = Math.floor((treeDigitNumber / 10) % 10)
+  let num_C = Math.floor(treeDigitNumber % 10)
   return Math.abs(num_B * 100 + num_A * 10 + num_C);
 }
 console.log(swapHundredsAndTens(123));
@@ -51,14 +51,16 @@ return Math.floor(seconds / 60 / 60);
 console.log(getFullHours(3600));
 // Задание 9 
 function getDayOfWeek(dayOFYear) {
-firstDay = 1
-weekDay = (firstDay + dayOFYear - 1) % 7
+let firstDay = 1
+let weekDay = (firstDay + dayOFYear - 1) % 7
 return (weekDay);
 }
 console.log(getDayOfWeek(365));
 // Задание 10
 function countSquares(A, B, C) {
-return Math.floor((A * B) / (C * C));
+let x = Math.floor(A / C)
+let y = Math.floor(B / C)
+return Math.floor( x * y);
 }
 console.log(countSquares(10, 10, 5));
 export { calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments, calculateDigitSum,  swapHundredsAndTens, getHundredsDigit, getFullHours, getDayOfWeek, countSquares };

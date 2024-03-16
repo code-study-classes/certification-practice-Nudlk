7398270dcdac8d203e1a5a6b64787caad347a199
Nudlk <159611741+Nudlk@users.noreply.github.com>
2024-03-16 11:02:26 +0300

    Update strings.js

--- strings.js
+++ strings.js
@@ -17,41 +17,41 @@ function countUppercaseLetters(str) {
 // Задание 2
 function combineStrings(N1, N2, S1, S2) {
     First = S1.slice(0, N1);
-    Second = S2.slice(-N2)
+    Second = S2.slice(-N2);
     return (First + Second);
 }
 
 console.log(combineStrings(3, 2, 'Hello', 'World'));
 // Задание 3
 function containsSubstring(S, S0) {
-    return S.includes(S0)
+    return S.includes(S0);
 }
 
 console.log(containsSubstring('Hello, World!', 'World'));
 // Задание 4
 function replaceSubstring(S, S1, S2) {
-    S0 = S.replace(S1, S2)
+    S0 = S.replace(S1, S2);
     return (S0);
 }
 
 console.log(replaceSubstring('Hello, World!', 'World', 'Universe'))
 // Задание 5
 function countWordsWithSameLetters(sentence) {
-    let count = 0
-    let words = sentence.split(" ")
+    let count = 0;
+    let words = sentence.split(" ");
  for (let i = 0; i < words.length; i++) {
         if (words[i][0].toLowerCase() === words[i][words[i].length - 1].toLowerCase()) {
             count++;
         }
     }
-    return (count)
+    return (count);
 }
 
 console.log(countWordsWithSameLetters('Racecar radar level Civic'));
 // Задание 6
 function countWordsWithA(sentence) {
-    let words = sentence.split(" ")
-    let count = 0
+    let words = sentence.split(" ");
+    let count = 0;
      for (let i = 0; i < words.length; i++) {
         if (words[i].includes('A') || words[i].includes('a')) {
             count++;
@@ -63,8 +63,8 @@ function countWordsWithA(sentence) {
 console.log(countWordsWithA('This is a sample sentence.'));
 // Задание 7
 function normalizeSpaces(sentence) {
-   fix = sentence.replace(/\s+/g, ' ')
-    return (fix)
+   fix = sentence.replace(/\s+/g, ' ');
+    return (fix);
 }
 
 console.log(normalizeSpaces('  This  is   a   sample    sentence. '));
